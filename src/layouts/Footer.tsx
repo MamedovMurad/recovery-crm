66ba771dd6109c25d339de1ebf367356fafee90b
MamedovMurad <muradvm@code.edu.az>
2024-05-30 06:04:57 -0700

    removed footer addition words

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,15 +4,9 @@ const Footer = () => {
     <footer className="footer h-16 flex items-center px-6 bg-white shadow dark:bg-gray-800">
       <div className="flex md:justify-between justify-center w-full gap-4">
         <div>
-          {new Date().getFullYear()} © Konrix - <a href="https://coderthemes.com/" target="_blank" rel="noreferrer">Coderthemes</a>
+          {new Date().getFullYear()} © CRM - <a href="https://recovery.az" target="_blank" rel="noreferrer">RECOVERY.AZ</a>
         </div>
-        <div className="md:flex hidden gap-4 item-center md:justify-end">
-          <a href="" className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">About</a>
-          <span className="border-e border-gray-300 dark:border-gray-700"></span>
-          <a href="" className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">Support</a>
-          <span className="border-e border-gray-300 dark:border-gray-700"></span>
-          <a href="" className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">Contact Us</a>
-        </div>
+ 
       </div>
     </footer>
   )
